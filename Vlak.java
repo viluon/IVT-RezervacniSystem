be3a6284ebfe8b10745cec14de7fa4eedfb019f4
Andrew Kvapil <viluon@espiv.net>
2016-02-24 12:47:19 +0100

    Finish Task 4

--- Vlak.java
+++ Vlak.java
@@ -5,10 +5,17 @@ public class Vlak
     private Trasa trasa;
     private int kapacita;
 
+    private int cenaRezervaci;
+    private String[][] evidenceRezervaci;
+
     public Vlak( String o, Trasa t, int k )
     {
         this.oznaceni = o;
         this.trasa = t;
         this.kapacita = k;
+
+        this.cenaRezervaci = 0;
+        this.evidenceRezervaci = null;
+        // this.evidenceRezervaci = new String[ this.kapacita ][ this.trasa.];
     }
 }
